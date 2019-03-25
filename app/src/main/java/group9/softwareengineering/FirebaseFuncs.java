60eb5ca3b7d0d13c3092be2892be550e7776d85d
sophianathanielle <45719926+sophianathanielle@users.noreply.github.com>
2019-03-25 18:56:07 +0000

    created getPostings()
    also Pet and Posting classes

--- app/src/main/java/group9/softwareengineering/FirebaseFuncs.java
+++ app/src/main/java/group9/softwareengineering/FirebaseFuncs.java
@@ -3,19 +3,20 @@ package group9.softwareengineering;
 
 import android.support.annotation.NonNull;
 import android.util.Log;
-import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
-import static com.firebase.ui.auth.AuthUI.TAG;
+import java.util.ArrayList;
+import java.util.List;
 
 public class FirebaseFuncs {
     private static final FirebaseFuncs ourInstance = new FirebaseFuncs();
@@ -54,5 +55,46 @@ public class FirebaseFuncs {
         db.collection("profiles").document(currentUser.getUid()).set(profile);
     }
 
+    private List<Posting> getPostings() {
+        final ArrayList<Posting> postings = new ArrayList<>();
 
+        db.collection("postings")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                Posting posting = document.toObject(Posting.class);
+                                if (!posting.getOwner_id().equals(currentUser.getUid())) {
+                                    for (String petID: posting.getPetIDs()) {
+                                        Pet pet = getPet(petID);
+                                        posting.addPet(pet);
+                                    }
+                                    postings.add(posting);
+                                }
+                            }
+                        } else {
+                            Log.d("FB", "Error getting documents: ", task.getException());
+                        }
+                    }
+                });
+
+        return postings;
+    }
+
+    private Pet getPet(String petID) {
+        final Pet[] pet = new Pet[1];
+
+        DocumentReference petRef = db.collection("pets").document(petID);
+
+        petRef.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+            @Override
+            public void onSuccess(DocumentSnapshot documentSnapshot) {
+                pet[0] = documentSnapshot.toObject(Pet.class);
+            }
+        });
+
+        return pet[0];
+    }
 }
---
+++ app/src/main/java/group9/softwareengineering/Pet.java
@@ -0,0 +1,56 @@
+package group9.softwareengineering;
+
+public class Pet {
+
+    private String id;
+    private String name;
+    private int age;
+    private String bio;
+    private String species;
+    private String breed;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getBio() {
+        return bio;
+    }
+
+    public void setBio(String bio) {
+        this.bio = bio;
+    }
+
+    public String getSpecies() {
+        return species;
+    }
+
+    public void setSpecies(String species) {
+        this.species = species;
+    }
+
+    public String getBreed() {
+        return breed;
+    }
+
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+}
