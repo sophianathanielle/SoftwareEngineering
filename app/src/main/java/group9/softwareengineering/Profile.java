9132c5907b83c1bb2fc61019bee728c5c3a69ace
sophianathanielle <45719926+sophianathanielle@users.noreply.github.com>
2019-03-29 14:59:25 +0000

    reverted changes (brought my code back)

--- app/src/main/java/group9/softwareengineering/Profile.java
+++ app/src/main/java/group9/softwareengineering/Profile.java
@@ -4,17 +4,12 @@ import android.location.Location;
 
 public class Profile {
 
-    private String name,phone_number,bio,email,password;
-    private int fee_ph,rating;
+    private String name;
+    private String phone_number;
+    private String bio;
+    private int fee_ph;
+    private int rating;
     private Location location;
-
-
-    public Profile(String email, String password, String name, String phone_number) {
-        this.name = name;
-        this.phone_number = phone_number;
-        this.email = email;
-        this.password = password;
-    }
 
     public String getName() {
         return name;
@@ -60,19 +55,5 @@ public class Profile {
         this.location = location;
     }
 
-    public String getEmail() {
-        return email;
-    }
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
