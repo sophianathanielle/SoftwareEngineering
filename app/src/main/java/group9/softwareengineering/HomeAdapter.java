b7c4392602749deef89fd6deab170afb61b0df77
Theodoros <tm00591@surrey.ac.uk>
2019-04-02 18:11:54 +0100

    created onclick for home recycler view

--- app/src/main/java/group9/softwareengineering/HomeAdapter.java
+++ app/src/main/java/group9/softwareengineering/HomeAdapter.java
@@ -1,6 +1,7 @@
 package group9.softwareengineering;
 
 import android.content.Context;
+import android.os.storage.OnObbStateChangeListener;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -16,15 +17,17 @@ import java.util.ArrayList;
 
 public class HomeAdapter extends RecyclerView.Adapter<HomeAdapter.ExampleViewHolder> {
     private ArrayList<Posting> postings;
+    private onClickListener onClickListener;
     private String pay , posted;
     private Context context;
 
 
-    public HomeAdapter(ArrayList<Posting> postings ,String pay ,String posted, Context context){
+    public HomeAdapter(ArrayList<Posting> postings ,String pay ,String posted, Context context, onClickListener onClickListener){
         this.postings = postings;
         this.pay = pay;
         this.posted = posted;
         this.context = context;
+        this.onClickListener = onClickListener;
     }
 
 
@@ -32,7 +35,7 @@ public class HomeAdapter extends RecyclerView.Adapter<HomeAdapter.ExampleViewHol
     @Override
     public ExampleViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
         View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.home_recyclerview_item , viewGroup , false);
-        ExampleViewHolder example = new ExampleViewHolder(view);
+        ExampleViewHolder example = new ExampleViewHolder(view, onClickListener);
         return example;
     }
 
@@ -53,20 +56,31 @@ public class HomeAdapter extends RecyclerView.Adapter<HomeAdapter.ExampleViewHol
 
 
 
-    public static class ExampleViewHolder extends RecyclerView.ViewHolder {
-        public TextView description;
-        public TextView payment;
-        public TextView posted;
-        public ImageView imageView;
+    public static class ExampleViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+         TextView description;
+         TextView payment;
+         TextView posted;
+         ImageView imageView;
+         onClickListener onClickListener;
 
-        public ExampleViewHolder(@NonNull View itemView) {
+        public ExampleViewHolder(@NonNull View itemView, onClickListener onClickListener) {
             super(itemView);
+            this.onClickListener = onClickListener;
             description = itemView.findViewById(R.id.description);
             payment = itemView.findViewById(R.id.payment);
             posted = itemView.findViewById(R.id.posted);
             imageView = itemView.findViewById(R.id.image);
-
+            itemView.setOnClickListener(this);
         }
+
+        @Override
+        public void onClick(View view) {
+            onClickListener.onItemClick(getAdapterPosition());
+        }
+    }
+
+    public interface onClickListener{
+        void onItemClick(int position);
     }
 
 }
