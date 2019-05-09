51221003c2393d8c71ed784dccf69141bf860d08
Justice Lock <jl01401@surrey.ac.uk>
2019-05-09 15:39:13 +0100

    RequestedLocationsViewActivity
    Zooms to accomodate all markers properly

--- app/src/main/java/group9/softwareengineering/RequestedLocationsViewActivity.java
+++ app/src/main/java/group9/softwareengineering/RequestedLocationsViewActivity.java
@@ -16,6 +16,7 @@ import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.Marker;
@@ -30,14 +31,17 @@ public class RequestedLocationsViewActivity extends AppCompatActivity implements
     private ArrayList<Marker> mMarkers = new ArrayList<>();
     private ArrayList<String> latitudes = new ArrayList<>();
     private ArrayList<String> longitudes = new ArrayList<>();
+    private ArrayList<LatLng> mLatLngs = new ArrayList<>();
 
     private GoogleMap mMap;
-    private Posting posting;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_requested_locations_view);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         latitudes = getIntent().getStringArrayListExtra("latitudes");
@@ -45,41 +49,41 @@ public class RequestedLocationsViewActivity extends AppCompatActivity implements
         for(int i = 0; i < latitudes.size(); i++ ){
             double latitude = Double.parseDouble(latitudes.get(i));
             double longitude = Double.parseDouble(longitudes.get(i));
-            GeoPoint location = new GeoPoint(latitude , longitude);
+            GeoPoint location = new GeoPoint(latitude, longitude);
             locations.add(location);
         }
-        Log.v("PostingLength", Integer.toString(locations.size()));
-
-
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
         this.mMap = googleMap;
-        googleMap.setMinZoomPreference(5);
+        googleMap.setMinZoomPreference(1);
         googleMap.setMaxZoomPreference(20);
         LatLng mLatLng;
         Marker marker;
-        for (GeoPoint location : locations) {
+        for (GeoPoint location : this.locations) {
+            Log.v("ReqLocLocations", "Latitude: " + Double.toString(location.getLatitude()) + "Longitude: " + Double.toString(location.getLongitude()));
             mLatLng = new LatLng(location.getLatitude(), location.getLongitude());
+            mLatLngs.add(mLatLng);
             marker = mMap.addMarker(new MarkerOptions().position(mLatLng));
             mMarkers.add(marker);
         }
+
         LatLngBounds.Builder builder = new LatLngBounds.Builder();
-        for (Marker m : mMarkers) {
-            builder.include(m.getPosition());
+        for (LatLng mll : this.mLatLngs) {
+            builder.include(mll);
         }
         LatLngBounds bounds = builder.build();
-        int padding = 0;
-        CameraUpdate cu = CameraUpdateFactory.newLatLngBounds(bounds, padding);
-        googleMap.moveCamera(cu);
+        int width = getResources().getDisplayMetrics().widthPixels;
+        int height = getResources().getDisplayMetrics().heightPixels;
+        int padding = (int) (width * 0.10);
+
+        CameraUpdate cu = CameraUpdateFactory.newLatLngBounds(bounds, width, height, padding);
+        mMap.animateCamera(cu);
     }
 
     public void stopActivity(View view) {
         this.finish();
     }
-
-
-
 
 }
